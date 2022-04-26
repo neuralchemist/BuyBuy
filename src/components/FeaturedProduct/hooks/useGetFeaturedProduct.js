6f52da7bdeccb855e256e084d4b995d136c7b1a3
neuralchemist <hacker.turab91@gmail.com>
2022-04-26 02:22:30 +0200

    removed React from hooks

--- src/components/FeaturedProduct/hooks/useGetFeaturedProduct.js
+++ src/components/FeaturedProduct/hooks/useGetFeaturedProduct.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { featuredProduct } from "../../DummyData/featuredProducts";
 
 function useGetFeaturedProduct() {
--- src/components/Products/hooks/useGetAllProducts.js
+++ src/components/Products/hooks/useGetAllProducts.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { products } from "../../DummyData/products";
 
 function useGetAllProducts() {
